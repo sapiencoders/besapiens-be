60b24fe328ffc3ae50c08470be28ccf2a33eb46b
Prakhar Saxena <prakhar.saxena@ubimaster.de>
2024-04-25 21:08:20 +0200

    first steps

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from blogs.models import Post
 
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'created_at', 'active']
     list_filter = ['active']
     search_fields = ['title']
+
 
 admin.site.register(Post, PostAdmin)
--- blogs/models.py
+++ blogs/models.py
@@ -3,22 +3,26 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from markdown import markdown
 
+
 def validate_file_extension(value):
     ext = os.path.splitext(value.name)[1]
     if ext not in ['.md', '.html']:
-        raise ValidationError('Unsupported file extension. Only .md or .html files are allowed.')
+        raise ValidationError(
+            'Unsupported file extension. Only .md or .html files are allowed.')
+
 
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
-    file = models.FileField(upload_to='blog_files/', validators=[validate_file_extension])
+    file = models.FileField(upload_to='blog_files/',
+                            validators=[validate_file_extension])
     active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
-    
+
     def clean(self):
         if not self.file and not self.content:
             raise ValidationError("Either 'file' or 'content' is required.")
